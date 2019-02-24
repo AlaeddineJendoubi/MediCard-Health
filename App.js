4dbd6ef70bf545fec882ec496b15d7f63afc96a6
wiemrh <ouiemm.je@gmail.com>
2019-02-24 20:21:49 +0100

    login with express , mysql

--- App.js
+++ App.js
@@ -1,29 +1,55 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import Accueil from './screens/Accueil'
-import Login from './screens/Login'
-import {createStackNavigator, createAppContainer} from 'react-navigation';
+import { StyleSheet, Text,Image , View , SafeAreaView, ScrollView ,Dimensions , Button} from 'react-native';
+import { createDrawerNavigator , createAppContainer , DrawerItems} from 'react-navigation'
+import RendezVous from './screens/RendezVous'
+import ParcoursDeSoin from './screens/ParcoursDeSoin'
+import Analyses from './screens/Analyses'
+import Profil from './screens/Profil'
+import APropos from './screens/APropos';
+
+const {width} = Dimensions.get('window')
 export default class App extends React.Component {
   render() {
     return (
      <MyApp />
+
     );
   }
 }
 
-const MainNavigator = createStackNavigator({
+const CustomDrawerComponent = (props) => (
 
-  Login: {screen: Login},
-  Accueil: {screen: Accueil},
-},
+<SafeAreaView style={{ flex: 1 }}>
+<View style= {{height:150 , backgroundColor:'white' , 
+alignItems: 'center' , justifyContent: 'center'}}>
+<Image source={require('./assets/wiem.png')} 
+style={{ height: 120 , width: 120 , borderRadius: 60}}/>
 
-{
-  mode: 'modal',
-  headerMode: 'none',
+</View>
+<ScrollView>
+  <DrawerItems {...props} />
+</ScrollView>
+</SafeAreaView>
+)
+
+
+const AppDrawerNavigator = createDrawerNavigator({
+ 'Rendez Vous' : RendezVous,
+ 'Parcours de soin': ParcoursDeSoin,
+ 'Analyses' : Analyses,
+ 'Profil' : Profil,
+ 'A Propos': APropos,
+
+}, {
+contentComponent : CustomDrawerComponent ,
+drawerWidth: width , 
+contentOptions : {
+  activeTintColor : 'orange'
 }
-);
+}
+)
 
-const MyApp = createAppContainer(MainNavigator);
+const MyApp = createAppContainer (AppDrawerNavigator);
 
 
 
--- screens/RendezVous.js
+++ screens/RendezVous.js
@@ -46,5 +46,7 @@ const styles =StyleSheet.create({
 
       flex: 1,
     
+    
+       
     }
 })
