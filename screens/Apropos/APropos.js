84b8ed5ffe2ccb76d1fbc57884e5c99796b422ab
wiemrh <ouiemm.je@gmail.com>
2019-04-22 20:23:18 +0200

    ordonance + profil + login final

--- screens/Apropos/APropos.js
+++ screens/Apropos/APropos.js
@@ -2,10 +2,15 @@ import React, { Component } from "react";
 import {
   View,
   Text,
-  StyleSheet
-} from "react-native";
-import {Left , Right, Icon, Header ,Body} from 'native-base'
+  StyleSheet , 
+  Image,
+  ImageBackground,
+  ScrollView
 
+} from 'react-native';
+  import logo from '../../assets/images/logo.png'
+import {Left , Right, Icon, Header } from 'native-base'
+import bg from "../../assets/images/c.jpg";
 
 class APropos extends Component {
 
@@ -18,27 +23,89 @@ class APropos extends Component {
 
   render() {
     return (
-
+      <ImageBackground source={bg}  style={styles.Backgroundcontainer}  > 
+     <ScrollView>
      <View style={ styles.container}>
-             <Header style={{marginTop:35}}>
-            <Left>
+          <Header style ={{backgroundColor : "#FFFFFF" }} >
+            <Left style ={{top:10 , flex:1 }} >
                  <Icon name ="menu"  onPress={() =>
                 this.props.navigation.openDrawer()
                 } />
              </Left>
-             <Right>
-             </Right>
-             <Body>
-             </Body>
-
          </Header>
 
-     <View style ={{ flex:1, alignItems:'center',justifyContent:'center'}}>
-           <Text> APropos </Text>
-     </View>
+         <View style={styles.logoContainer}>
+      <Image  source = {logo} style = {styles.logo}  />
+      <Text style = {{  
+        fontSize:22, fontWeight:'600' ,marginTop : 5}}> 
+       Votre dossier medical de poche 
+        </Text>
+</View>
+<View style={styles.logoContainer}>
+        <Text style = {{  fontSize:20, fontWeight:'500' ,margin :3 , textDecorationLine : 'underline' }}> 
+     Contact 
+        </Text>
 
-     </View>
 
+        <Text style = {{  fontSize:20, fontWeight:'400' ,  
+     margin : 5 }}> 
+              Ariana,Tunis 
+        </Text>
+
+        <Text style = {{  fontSize:20, fontWeight:'400' ,  
+    margin : 5 }}> 
+              mediCard@esprit.tn 
+        </Text>
+
+        <Text style = {{  fontSize:20, fontWeight:'400', 
+   margin : 5  }}> 
+              +216 22 222 222
+        </Text>
+
+        <Text style = {{  fontSize:20, fontWeight:'500' ,margin :10, textDecorationLine : 'underline' }}> 
+     Equipe
+        </Text>
+
+
+
+        <View style = {{  flexDirection: 'row'  }} >
+       <View >
+       <Image style={styles.avatar}source={{uri: 'https://bootdey.com/img/Content/avatar/avatar6.png'}}/>
+       <Image style={styles.avatar}source={{uri: 'https://bootdey.com/img/Content/avatar/avatar6.png'}}/>
+       <Image style={styles.avatar}source={{uri: 'https://bootdey.com/img/Content/avatar/avatar6.png'}}/>
+       <Image style={styles.avatar}source={{uri: 'https://bootdey.com/img/Content/avatar/avatar6.png'}}/>
+
+       </View>
+
+       <View  >
+       <Text style = {styles.equipeText}> 
+         Yosra bouajaja
+        </Text>
+        <Text style = {styles.equipeText}> 
+             Wiem rebah
+        </Text>
+        <Text style = {styles.equipeText}> 
+            Ala eddine jendoubi 
+        </Text>
+        <Text style = {styles.equipeText}> 
+             takwa hedhly
+        </Text>
+       
+       </View>
+      
+
+       </View>
+       
+
+
+       </View>
+
+      
+
+      
+     </View>
+     </ScrollView>
+    </ImageBackground>
     );
   }
 
@@ -49,9 +116,42 @@ export default APropos;
 const styles =StyleSheet.create({
     container : {
 
-      flex: 1,
+      flex: 1,  }
 
+      ,
 
+  logoContainer:{
+   
+    alignItems : 'center'
+    , marginTop : 20
+  } ,
 
-    }
+  logo:{
+    width:160,
+    height:100 
+  
+  },
+  avatar: {
+    width: 50,
+    height: 50,
+    borderRadius: 63,
+    borderWidth: 4,
+    borderColor: "white",
+    marginTop:1,
+  }, 
+  equipeText:{
+    fontSize:20, 
+    fontWeight:'400',  
+    marginTop : 3  , 
+    height:50 
+
+  }  ,
+
+  Backgroundcontainer :{
+    flex : 1 ,
+   
+  },
+
+  
+
 })
--- screens/Ordonnance/Ordonnance.js
+++ screens/Ordonnance/Ordonnance.js
@@ -2,26 +2,137 @@ import React, { Component } from "react";
 import {
   View,
   Text,
-  StyleSheet
+  StyleSheet,
+  FlatList,
+  ImageBackground
 } from "react-native";
-import {Left , Right, Icon, Header } from 'native-base'
+import { Left, Right, Icon, Header, Card, CardItem, Body } from 'native-base'
+import bg from "../../assets/images/c.jpg";
+//import { Card } from 'react-native-elements';
 
 
 class Ordonnance extends Component {
-   navigationOptions ={
-    drawerIcon: ({tintColor}) => (
-      <Icon name = "folder" style ={{ fontSize : 24 ,
-        color:tintColor}}/>
-      )
+  constructor() {
+    super()
+    this.state = {
+      data: []
+    }
   }
-  render(){
-    return(
-      <View style={{alignItems:'center',fontSize:50}}>
-        <Text>
-          this is Ordonnance
-        </Text>
+
+
+  renderItem = ({ item }) => {
+    return (
+
+
+      <View>
+
+
+        <Card >
+      
+         <Header  style={{backgroundColor: "#87cefa"}}>
+         <Text> Ordonnance de docteur : {item.fnmedecin} {item.lnmedecin}  </Text>
+     
+         </Header>
+          <CardItem>
+            <Body>
+            <Text>
+             
+               Liste des medicaments : {item.medicaments}
+                </Text>
+            </Body>
+          </CardItem>
+          <CardItem footer>
+          <Text>  date : {item.dateconsulation} </Text>
+          </CardItem>
+        </Card>
+
+
+
+
+
+
+
+
       </View>
+    )
+  }
+
+
+
+
+
+
+
+
+
+
+  componentDidMount() {
+    // this.fetchData();
+    const code = global.code;
+    const url = "http://192.168.43.33:3000/users/ordonnances/" + code
+    fetch(url)
+      .then((response) => response.json())
+      .then((responseJson) => {
+        this.setState({
+          data: responseJson
+        })
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+  }
+
+
+
+  static navigationOptions = {
+    drawerIcon: ({ tintColor }) => (
+      <Icon name="list" style={{
+        fontSize: 24,
+        color: tintColor
+      }} />
+    )
+  }
+  
+  render() {
+    return (
+      <ImageBackground source={bg}  style={styles.Backgroundcontainer}  >
+      <View style={styles.container}>
+    
+      <Header style ={{backgroundColor : "#FFFFFF" }} >
+          <Left style ={{top:10 , flex:1 }} >
+            <Icon name ="menu"  onPress={() =>
+                        this.props.navigation.openDrawer()} />
+            </Left>
+         
+          </Header>
+        <View>
+          <FlatList
+            data={this.state.data}
+            renderItem={this.renderItem}
+            keyExtractor={(item, index) =>
+              index.toString()}
+          />
+
+        </View>
+      
+      </View>
+      </ImageBackground>
+
     );
   }
+
 }
+
 export default Ordonnance;
+
+const styles = StyleSheet.create({
+  container: {
+
+    flex: 1,}
+    ,
+
+  Backgroundcontainer :{
+    flex : 1 ,
+   
+  },
+})
